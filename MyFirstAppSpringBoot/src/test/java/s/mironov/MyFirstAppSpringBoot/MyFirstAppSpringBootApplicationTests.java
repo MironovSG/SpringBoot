cbc1adc5c547ab202d81b770acc02119eaa738e1
sergey <xy2vdd@gmail.com>
2023-09-20 00:25:30 +0500

    Initial commit

---
+++ MyFirstAppSpringBoot/src/test/java/s/mironov/MyFirstAppSpringBoot/MyFirstAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package s.mironov.MyFirstAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
